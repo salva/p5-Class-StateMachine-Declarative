96e9480d5ead0aad63e6e509f3785cc522150100
Salvador <sfandino@yahoo.com>
2013-04-01 19:07:37 +0200

    working on it

--- lib/Class/StateMachine/Declarative.pm
+++ lib/Class/StateMachine/Declarative.pm
@@ -8,11 +8,10 @@ use 5.010;
 
 use strict;
 use warnings;
-
 use Carp;
 BEGIN { our @CARP_NOT = qw(Class::StateMachine Class::StateMachine::Private) }
-use Class::StateMachine;
-use mro;
+
+use Class::StateMachine::Declarative::Builder;
 
 my $dump = exists $ENV{CLASS_STATEMACHINE_DECLARATIVE_DUMPFILE};
 my %dump;
@@ -28,28 +27,13 @@ END {
 
 sub import {
     shift;
-    _init_class(scalar(caller), @_);
-}
-
-sub _init_class {
-    my $class = shift;
+    my $class = scalar(caller);
+    $dump{$class} = [ @_ ] if $dump;
     my $builder = Class::StateMachine::Declarative::Builder->new($class);
-    _init_states($builder, undef, @_);
-}
-
-sub _init_states {
-    my ($builder, $parent, @states) = @_;
-    while (@states) {
-        my $name = shift @states;
-        my %decl = %{shift(@states)};
-        my %args = map { $_ => delete $decl{$_} } qw(enter leave delay ignore transitions);
-        my $substates = delete $decl{substates};
-        %decl and croak "bad state declaration argument(s) (".join(", ", keys %decl).")";
-        my $state = $builder->new_state($name, $parent, %args);
-        _init_states($builder, $states, @$substates);
-    }
-    $builder->resolve_transitions;
-    $builder->generate;
+    $builder->parse_state_declarations(@_);
+    $builder->generate_class;
+    use Data::Dumper;
+    print STDERR Dumper($builder);
 }
 
 1;
@@ -67,13 +51,11 @@ Class::StateMachine::Declarative - Perl extension for blah blah blah
   use parent 'Class::StateMachine';
 
   use Class::StateMachine::Declarative
-      __any__  => { enter       => sub { say "entering state $_[0]" },
-                    leave       => sub { say "leaving state $_[0" },
       happy    => { transitions => { on_knocked_down => 'injuried',
                                      on_kicked       => 'angry' } },
       injuried => { transitions => { on_sleep        => 'happy' } },
-      angry    => { 'enter+'    => sub { shift->bark },
-                    'leave+'    => sub { shift->bark },
+      angry    => { enter       => sub { shift->bark },
+                    leave       => sub { shift->bark },
                     transitions => { on_feed         => 'happy',
                                      on_knocked_down => 'injuried' } } };
 
@@ -114,7 +96,7 @@ L<Class::StateMachine>.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2011 by Salvador FandiE<ntilde>o <sfandino@yahoo.com>
+Copyright (C) 2011-2013 by Salvador FandiE<ntilde>o <sfandino@yahoo.com>
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.12.4 or,
--- lib/Class/StateMachine/Declarative/Builder.pm
+++ lib/Class/StateMachine/Declarative/Builder.pm
@@ -6,6 +6,16 @@ use Carp;
 use 5.010;
 use Scalar::Util ();
 
+use Class::StateMachine;
+*debug = \$Class::StateMachine::debug;
+our $debug;
+
+sub _debug {
+    my $n = shift;
+    warn "@_\n" if $debug and $n;
+}
+
+
 sub new {
     my ($class, $target_class) = @_;
     my $top = Class::StateMachine::Declarative::Builder::State->_new;
@@ -21,54 +31,104 @@ sub _bad_def {
     croak "@msg on definition of state '$state->{name}' for class '$self->{class}'";
 }
 
-sub new_state {
+sub _ref_to_pair_list {
+    my $ref = shift;
+    my @l;
+    do {
+        local ($@, $SIG{__DIE__});
+        eval { @l = @$ref; 1 } or eval { @l = %$ref; 1 }
+    } or croak "ARRAY or HASH ref expected";
+    @l & 1 and croak "odd number of elements in list";
+    @l;
+}
+
+sub _ref_is_ordered_list {
+    my $ref = shift;
+    local ($@, $SIG{__DIE});
+    eval { @$ref || 1 };
+}
+
+
+sub _ensure_list {
+    my $ref = shift;
+    ( UNIVERSAL::isa($ref, 'ARRAY') ? @$ref : $ref );
+}
+
+sub parse_state_declarations {
+    my $self = shift;
+    $self->_parse_state_declarations($self->{top}, @_);
+    $self->_resolve_transitions($self->{top}, []);
+}
+
+sub _parse_state_declarations {
+    my $self = shift;
+    my $parent = shift;
+    while (@_) {
+        my $name = shift;
+        my $decl = shift // {};
+        $self->_add_state($name, $parent, _ref_to_pair_list($decl));
+    }
+}
+
+sub _add_state {
     my ($self, $name, $parent, @decl) = @_;
 
-    $parent //= $self->{top};
     my $state = Class::StateMachine::Declarative::Builder::State->_new($name, $parent);
 
     while (@decl) {
         my $k = shift @decl;
-        my $v = shift @decl;
-        if (defined $v) {
-            given ($k) {
-                when ('enter') {
-                    ref $v and $self->_bad_def($state, "'enter' arguments is not an scalar");
-                    $state->{enter} = $v;
-                }
-                when ('leave') {
-                    ref $v and $self->_bad_def($state, "'leave' arguments is not an scalar");
-                    $state->{leave} = $v;
-                }
-                when ('delay') {
-                    ref $v eq 'ARRAY' or $self->_bad_def($state, "'delay' arguments is not an ARRAY reference");
-                    push @{$state->{delay}}, @$v;
-                }
-                when ('ignore') {
-                    ref $v eq 'ARRAY' or $self->_bad_def($state, "'ignore' arguments is not an ARRAY reference");
-                    push @{$state->{ignore}}, @$v;
-                }
-                when ('transitions') {
-                    ref $v eq 'HASH' or $self->_bad_def($state, "'transitions' arguments is not a HASH reference")
-                    while (my ($event, $target) = each %$v) {
-                        ref $event and $self->_bad_def($state, "transition event arguments is not an scalar");
-                        ref $target and $self->_bad_def($state, "transition target arguments is not an scalar");
-                        $state->{transitions}{$event} = $target if defined $target;
-                    }
-                }
-                default {
-                    $self->_bad_def($state, "unsupported state attribute '$k'");
-                }
-            }
+        my $method = $self->can("_handle_attr_$k") or $self->_bad_def($state, "bad declaration '$k'");
+        if (defined (my $v = shift @decl)) {
+            _debug(16, "calling handler for attribute $k with value $v");
+            $method->($self, $state, $v);
         }
     }
+    $self->{states}{$state->{full_name}} = $state;
     $state;
 }
 
-sub resolve_transitions {
-    my $self = shift;
-    my @path;
-    $self->_resolve_transitions($self->{top}, \@path);
+sub _handle_attr_enter {
+    my ($self, $state, $v) = @_;
+    $state->{enter} = $v;
+}
+
+sub _handle_attr_leave {
+    my ($self, $state, $v) = @_;
+    $state->{leave} = $v;
+}
+
+sub _handle_attr_jump {
+    my ($self, $state, $v) = @_;
+    $state->{jump} = $v;
+}
+
+
+sub _handle_attr_delay {
+    my ($self, $state, $v) = @_;
+    push @{$state->{delay}}, _ensure_list($v);
+}
+
+sub _handle_attr_ignore {
+    my ($self, $state, $v) = @_;
+    push @{$state->{ignore}}, _ensure_list($v);
+}
+
+sub _handle_attr_transitions {
+    my ($self, $state, $v) = @_;
+    my @transitions = _ref_to_pair_list($v);
+    while (@transitions) {
+        my $event = shift @transitions;
+        my $target = shift @transitions;
+        $state->{transitions}{$event} = $target if defined $target;
+    }
+}
+
+sub _handle_attr_substates {
+    my ($self, $state, $v) = @_;
+    $self->_parse_state_declarations($state, _ref_to_pair_list($v));
+    if (_ref_is_ordered_list($v)) {
+        $state->{substates_are_ordered} = 1;
+    }
 }
 
 sub _resolve_transitions {
@@ -84,22 +144,30 @@ sub _resolve_transitions {
     $state->{transitions_abs} = \%transitions_abs;
     $state->{transitions_rev} = \%transitions_rev;
 
-    for my $substate (values %{$state->{}}) {
-        $self->_resolve_transitions($self, $substate, \@path);
+    for my $substate (@{$state->{substates}}) {
+        $self->_resolve_transitions($substate, \@path);
     }
 }
 
 sub _resolve_target {
     my ($self, $target, $path) = @_;
-    if ($target =~m|^/|) {
+    # _debug(32, "resolving target '$target' from '".join('/',@$path)."'");
+    if ($target =~ m|^__(\w+)__$|) {
+        return $target;
+    }
+    if ($target =~ m|^/|) {
         return $target if $self->{states}{$target};
+        _debug(32, "absolute target '$target' not found");
     }
     else {
         my @path = @$path;
         while (@path) {
             my $target_abs = join('/', @path, $target);
-            return $target_abs if $self->{states}{$target_abs};
-            shift @path;
+            if ($self->{states}{$target_abs}) {
+                _debug(32, "target '$target' from '".join('/',@$path)."' resolved as '$target_abs'");
+                return $target_abs;
+            }
+            pop @path;
         }
     }
 
@@ -109,11 +177,12 @@ sub _resolve_target {
 }
 
 my $ignore_cb = sub {};
+my $goto_next_state = sub { $_[0]->state($_[0]->next_state) };
 
-sub generate {
+sub generate_class {
     my $self = shift;
     my $class = $self->{class};
-    while (my ($full_name, $state) = each %{self->{states}}) {
+    while (my ($full_name, $state) = each %{$self->{states}}) {
         my $name = $state->{name};
         my $parent = $state->{parent};
         if ($parent and $parent != $self->{top}) {
@@ -135,16 +204,40 @@ sub generate {
                                                 $name);
         }
         for my $ignore (@{$state->{delay}}) {
-            Class::StateMachine::install_method($class, $event, $ignore_cb, $name);
+            Class::StateMachine::install_method($class, $ignore, $ignore_cb, $name);
         }
 
         while (my ($target, $events) = each %{$state->{transitions_rev}}) {
-            my $target_state = $self->{states}{$target};
-            my $method = $target_state->{come_here_method} //= do {
-                my $target_name = $target_state->{name};
-                sub { shift->state($name) };
-            };
+            my $method;
+            if ($target eq '__next__') {
+                $method = $goto_next_state;
+            }
+            else {
+                my $target_state = $self->{states}{$target};
+                $method = $target_state->{come_here_method} //= do {
+                    my $target_name = $target_state->{name};
+                    sub { shift->state($target_name) }
+                };
+            }
             Class::StateMachine::install_method($class, $_, $method, $name) for @$events;
+        }
+
+        my @ss = @{$state->{substates}};
+        if ($state->{substates_are_ordered}) {
+            my $current = shift @ss;
+            while (defined(my $next = shift @ss)) {
+                my $next_state = $next->{name};
+                Class::StateMachine::install_method($class, 'next_state', sub { $next_state }, $current->{name});
+                $current = $next_state;
+            }
+        }
+        else {
+            for my $ss (@ss) {
+                my $state_name = $ss->{name};
+                Class::StateMachine::install_method($class, 'next_state',
+                                                    sub { croak "next state not defined in state '$state_name'" },
+                                                    $ss->{name});
+            }
         }
     }
 }
@@ -167,7 +260,7 @@ sub _new {
     bless $state, $class;
     push @{$parent->{substates}}, $state if $parent;
     Scalar::Util::weaken($state->{parent});
-    $self;
+    $state;
 }
 
 1;
